98b011cf50af7c8cfb0b0f143ec5b002da5968a0
danya <danya.vishnyakov.2006@mail.ru>
2026-02-18 10:59:41 +0300

    Добавлено решение задания №1 с определением ассимптотической сложности

--- src/main/java/tasks/Task1.java
+++ src/main/java/tasks/Task1.java
@@ -2,9 +2,13 @@ package tasks;
 
 import common.Person;
 import common.PersonService;
+
+import java.time.Instant;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Задача 1
@@ -16,13 +20,25 @@ import java.util.Set;
 public class Task1 {
 
   private final PersonService personService;
-
+  /*
+    Асимптотическая сложность O(n*m), где n - размер листа с индексами,
+    а m - размер множества person.
+    Если бы была другая структура данных, вместо set, к примеру map,
+    то можно было по ключу искать без перебора за O(1),
+    где ключ id person, доставать элементы, но так как у нас только set и list даны,
+    то пользуемся тем, что есть))
+   */
   public Task1(PersonService personService) {
     this.personService = personService;
   }
-
   public List<Person> findOrderedPersons(List<Integer> personIds) {
     Set<Person> persons = personService.findPersons(personIds);
-    return Collections.emptyList();
+    return personIds.stream().map(x->findPersonById(persons,x)).toList();
+  }
+  private Person findPersonById(Set<Person> persons,Integer id){
+    return persons.stream()
+            .filter(person->person.id().equals(id))
+            .findFirst()
+            .orElse(null);
   }
 }
